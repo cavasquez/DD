adf3c64612b959b1a191fe14f8ae892befef0ec4
Carlos Vasquez <cavasquez@ufl.edu>
2013-02-07 08:53:35 -0800

    Create NetworkSystem.java

---
+++ src/DD/Network/NetworkSystem.java
@@ -0,0 +1,13 @@
+package DD.Network;
+
+import java.util.ArrayList;
+
+import DD.Chat.ChatSystem;
+
+/*****************************************************************************************************
+* NetworkSystem will be an interface through which GameSystem, ChatSystem and any other System
+* will be able to communicate with ClientSystem or ServerSystem.
+******************************************************************************************************/
+
+
+
