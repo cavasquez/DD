74a9ed444d1c4f6973a9a33968601560c7b63677
Carlos Vasquez <cavasquez@ufl.edu>
2013-02-08 12:04:24 -0500

    Update src/DD/Network/Listener.java

--- src/DD/Network/Listener.java
+++ src/DD/Network/Listener.java
@@ -1,3 +1,55 @@
+package DD.Network;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Queue;
+import DD.Network.Message.NetworkMessage;
+
 /*****************************************************************************************************
-* Listener will contain the basic tools necessarry for the Server or Client to listen for messages.
+* Listener will be used to listen for messages from the Server or client. This will provide the basic 
+* tools used by ServerListener and ClientListener.
+* 
+* Listener will need to utilize ServerSocket
 ******************************************************************************************************/
+
+public abstract class DDSocket extends Thread
+{
+
+        /************************************ Class Attributes *************************************/
+	protected Socket socket = null;
+	protected ObjectInputStream input = null;
+	protected ObjectOutputStream output = null;
+	
+	/************************************ Class Methods *************************************/
+	public Listener(Socket socket, int socketID)
+	{
+		super(socket, socketID);
+	} /* end Server constructor */
+	
+	public void run() {} /* end run method */
+	
+	protected NetworkMessage getSocketMessage()
+	{
+		NetworkMessage message = null;
+		
+		try
+		{
+			message = (NetworkMessage) input.readObject();
+		} /* end try */
+		catch(IOException | ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		} /* end catch */
+		
+		return(message);
+	} /* end getSocketMessage method */
+	
+	protected void getMessage(NetworkMessage message)
+	{ /* We got a message from the stream. Process it. */
+		NetworkSystem.getInstance().getMessage(socketID, message);
+		
+	} /* end getMessage method */
+
+} /* end Listener class */
