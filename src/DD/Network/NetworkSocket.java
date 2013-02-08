becacd663513e851fe3b07c982969372f32c5ed8
Carlos Vasquez <cavasquez@ufl.edu>
2013-02-08 11:59:39 -0500

    Update src/DD/Network/NetworkSocket.java
    created NetworkSocket

--- src/DD/Network/NetworkSocket.java
+++ src/DD/Network/NetworkSocket.java
@@ -8,20 +8,62 @@ import java.util.Queue;
 import DD.Network.Message.NetworkMessage;
 
 /*****************************************************************************************************
-* DDSocket is an experimental socket that will attempt to use one thread to control both listening
-* and sending.
+* NetworkSocket will contain the basic tools that Listener and Client will be using.
 ******************************************************************************************************/
 
-public abstract class DDSocket extends Thread
+public abstract class NetworkSocket extends Thread
 {
 
-/************************************ Class Attributes *************************************/
-protected Socket socket = null;
-protected ObjectInputStream input = null;
-protected ObjectOutputStream output = null;
-protected volatile boolean sending;
-protected volatile boolean done;  /* done should be called when thread is to be closed */
-protected boolean working;	/* thread is doing work */
-protected int socketID;	/* Unique ID for thread */
-protected volatile Queue<NetworkMessage> messageList;	/* messages to be sent */
+        /************************************ Class Attributes *************************************/
+	protected Socket socket = null;
+	protected ObjectInputStream input = null;
+	protected ObjectOutputStream output = null;
+	protected int socketID;					/* Unique ID for thread */
+        protected boolean done;                                 /* Thread is done */
+	
+	/************************************ Class Methods *************************************/
+	public NetworkSocket(Socket socket, int socketID)
+	{
+		super("DDSocketThread" + Integer.toHexString(socketID));
+		this.socket = socket;
+		this.socketID = socketID;
+                this.done = false;
+	} /* end Server constructor */
+	
+	public void run() {} /* end run method */
+	
+	protected void createStreams()
+	{
+		try 
+		{
+			input = new ObjectInputStream(socket.getInputStream());
+			output = new ObjectOutputStream(socket.getOutputStream());
+		} /* end try */ 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} /* end catch */
+	} /* end createStreams method */
 
+	protected void closeStreams()
+	{
+		try 
+		{
+			input.close();
+			output.close();
+		} /* end try */ 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} /* end catch */
+		
+	} /* end closeServerSocket method */
+
+	public void close()
+	{
+		/* thread is done */
+		done = true;
+	} /* end close method */
+} /* end DDSocket */
