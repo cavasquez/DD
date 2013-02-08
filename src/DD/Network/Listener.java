09f72ed9206828d60274bc76c919410e5c01c979
Carlos Vasquez <cavasquez@ufl.edu>
2013-02-08 08:50:54 -0800

    Create Listener.java
    created Listener

---
+++ src/DD/Network/Listener.java
@@ -0,0 +1,4 @@
+/*****************************************************************************************************
+* InitialMessage is the first message sent to the server or the client and validates if the
+* client has a valid username.
+******************************************************************************************************/
